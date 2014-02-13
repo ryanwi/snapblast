da7de290223456bfe0e361c53318f7599136baae
Ryan Williams <rwilliamz@gmail.com>
2014-02-12 21:31:09 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Snapblast::Application.config.secret_key_base = '872dad5286fdbf72c4d7d9cbe78b30b8218a1c370fe1f4931d577513ad6aca61a94f788aac5ce0171a9a2d69eb57ca772d5b3a2de5624a7d28c88f59b5dc9cab'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Snapblast::Application.config.session_store :cookie_store, key: '_snapblast_session'
