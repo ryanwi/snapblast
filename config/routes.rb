e6099a3a9d356b103f4c9e7501e754b21ab7a2c2
Ryan Williams <rwilliamz@gmail.com>
2014-02-13 07:49:27 -0800

    Add about and contact content pages and wire up layout.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Snapblast::Application.routes.draw do
-  # get "welcome/index"
   resources :logins
 
+  get '/about' => 'welcome#about'
+  get '/contact' => 'welcome#contact'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
