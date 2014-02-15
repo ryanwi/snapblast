d85b0441cece358e708d0c369f7adb931b36c96f
Ryan Williams <rwilliamz@gmail.com>
2014-02-15 15:10:24 -0800

    Implement twilio calls and improve caching.

--- app/controllers/calls_controller.rb
+++ app/controllers/calls_controller.rb
@@ -1,48 +1,93 @@
+require 'uri'
+
 class CallsController < ApplicationController
   before_action :require_login
+  before_action :set_ts_client
 
   def new
-    ts_client = TeamsnapClient.new
+    @team_id = params[:id]
 
     # Get Team information
-    # store team response in cache and scope it to the user
-    team_cache_key = "#{session[:ts_token]}_team_#{params[:id]}"
-    @team = Rails.cache.fetch team_cache_key, :expires_in => 1.hour do
-      team_response = ts_client.get("teams/#{params[:id]}", headers:
-        { "X-Teamsnap-Token" => session[:ts_token] })
+    # cache team response and scope it to the user's session
+    team_resource = "teams/#{@team_id}"
+    team_key = "#{session[:ts_token]}_#{team_resource}"
+    @team = Rails.cache.fetch team_key, :expires_in => 10.minutes do
+      team_response = @ts_client.get(team_resource,
+        headers: { "X-Teamsnap-Token" => session[:ts_token] })
       JSON.parse team_response.body
     end
 
-    # Get Roster information
-    # store roster response in cache and scope it to the user
-    roster_cache_key = "#{session[:ts_token]}_roster_#{params[:id]}"
-    @roster = Rails.cache.fetch roster_cache_key, :expires_in => 1.hour do
-      roster_response = ts_client.get("teams/#{params[:id]}/as_roster", headers:
-        { "X-Teamsnap-Token" => session[:ts_token] })
-      JSON.parse roster_response.body
+    # Determine roster_id to use
+    # cache response scoped to the user's session
+    as_roster_resource = "teams/#{@team_id}/as_roster"
+    as_roster_key = "#{session[:ts_token]}_#{as_roster_resource}"
+    @as_roster = Rails.cache.fetch as_roster_key, :expires_in => 10.minutes do
+      as_roster_response = @ts_client.get(as_roster_resource,
+        headers: { "X-Teamsnap-Token" => session[:ts_token] })
+      JSON.parse as_roster_response.body
     end
+
+    # Just taking first for now, may need to prompt or be smarter about picking
+    @roster_id = @as_roster.first['roster']['id']
+    @team_rosters = get_team_rosters(@team_id, @roster_id)
   end
 
   def create
-    # collect phone numbers from roster, which should be cached
-    roster_cache_key = "#{session[:ts_token]}_roster_#{params[:team_id]}"
-    @roster = Rails.cache.fetch roster_cache_key, :expires_in => 1.hour do
-      roster_response = ts_client.get("teams/#{params[:id]}/as_roster", headers:
-        { "X-Teamsnap-Token" => session[:team_id] })
-      JSON.parse roster_response.body
-    end
+    team_id = params[:team_id]
+    roster_id = params[:roster_id]
 
+    # collect phone numbers from team roster
+    @team_rosters = get_team_rosters(team_id, roster_id)
     phone_numbers = []
-    @roster.each do |roster|
+    @team_rosters.each do |roster|
       roster["roster"]["roster_telephone_numbers"].each do |phone|
-        phone_numbers << phone["phone_number"]
+        phone_numbers << phone["phone_number"] unless phone["phone_number"].nil?
       end
     end
 
-    STDERR.puts phone_numbers.inspect
+    # Build message
+    message = URI.escape params[:message]
 
+    # Make the calls
+    # TODO: queue calls with Resque/DelayedJob
+    # TODO: log the calls to the database
+    twilio = Twilio::REST::Client.new ENV["TWILIO_ACCOUNT_SID"], ENV["TWILIO_AUTH_TOKEN"]
+    phone_numbers.each do |phone_number|
+      begin
+        puts "==================== calling"
+        puts "#{phone_number}"
+        call = twilio.account.calls.create({
+          :from => ENV["TWILIO_FROM"],
+          :to => phone_number,
+          :url => "http://twimlets.com/message?Message%5B0%5D=#{message}"
+          # :status_callback => "http://requestb.in/11k3d6m1"
+        })
+      rescue Twilio::REST::RequestError => e
+        puts "==================== error"
+        puts e.message
+      end
+    end
 
-    # flash
-    redirect_to "/teams"
+    redirect_to "/teams", notice: "Your calls have been made"
   end
+
+  private
+
+    # Setup TeamSnap client which is common for all actions
+    def set_ts_client
+      @ts_client = TeamsnapClient.new
+    end
+
+    # Get Complete Roster information for the team
+    def get_team_rosters(team_id, as_roster_id)
+      # cache roster response and scope it to the user's session
+      team_rosters_resource = "teams/#{team_id}/as_roster/#{as_roster_id}/rosters"
+      team_rosters_key = "#{session[:ts_token]}_#{team_rosters_resource}"
+      team_rosters = Rails.cache.fetch team_rosters_key, :expires_in => 10.minutes do
+        team_rosters_response = @ts_client.get(team_rosters_resource,
+          headers: { "X-Teamsnap-Token" => session[:ts_token] })
+        JSON.parse team_rosters_response.body
+      end
+    end
+
 end
--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -5,9 +5,15 @@ class TeamsController < ApplicationController
   # GET /teams.json
   def index
     # Get a list of teams for the user from the TeamSnap API
+    # cache response and scope it to the user's session
     ts_client = TeamsnapClient.new
-    response = ts_client.get("teams", headers: { "X-Teamsnap-Token" => session[:ts_token] })
-    @teams = JSON.parse response.body
+    teams_resource = "teams"
+    teams_key = "#{session[:ts_token]}_#{teams_resource}"
+    @teams = Rails.cache.fetch teams_key, :expires_in => 1.minute do
+      response = ts_client.get(teams_resource,
+        headers: { "X-Teamsnap-Token" => session[:ts_token] })
+      JSON.parse response.body
+    end
   end
 
 end
