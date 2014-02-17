881af0eeea427c3f9ee1bcba9e786373c48be2c2
Ryan Williams <rwilliamz@gmail.com>
2014-02-16 20:11:56 -0800

    Tests for welcome controller

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -4,8 +4,35 @@ describe WelcomeController do
 
   describe "GET 'index'" do
     it "returns http success" do
-      get 'index'
+      get :index
       response.should be_success
+    end
+
+    it "renders the index template" do
+      get :index
+      expect(response).to render_template("index")
+    end
+
+    it "renders the landing layout" do
+      get :index
+      response.should render_template("layouts/landing")
+    end
+  end
+
+  describe "GET 'about'" do
+    it "returns http success" do
+    get :about
+      response.should be_success
+    end
+
+    it "renders the about template" do
+      get :about
+      expect(response).to render_template("about")
+    end
+
+    it "renders the application layout" do
+      get :about
+      response.should render_template("layouts/application")
     end
   end
 
